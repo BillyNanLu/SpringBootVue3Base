d85278dd2dde3d5d9de46d3202da504ed6b35ad4
BillyNan_Lu <lunan96789@gmail.com>
2025-09-01 14:53:57 +0800

    add breaking-news module

---
+++ breaking-news/src/main/java/com/nan19/BreakingNewsApplication.java
@@ -0,0 +1,17 @@
+package com.nan19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class BreakingNewsApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BreakingNewsApplication.class, args);
+    }
+}
